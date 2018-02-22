60dc7b8ea3a1a1231e753eae4beed431b39ac6d3
Chew Kim Mey <chew.kim.mey@ucts.edu.my>
2018-02-22 14:55:17 +0800

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -5,7 +5,7 @@ int main()
 	int ang1, ang2, ang3; /*are three angles of a triangle  */
 
 						  /* Read two angles of the triangle from user separated by comma*/
-	printf("Input two angles of triangle separated by comma : ");
+	printf("Please Input two angles of triangle separated by comma : ");
 	scanf("%d, %d", &ang1, &ang2);
 
 	ang3 = 180 - (ang1 + ang2);  /* Calculates the third angle  */
